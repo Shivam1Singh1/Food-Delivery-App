b9e7a1608ce1e0a89637d955ff5b2128cbdeecd1
Shivam1Singh1 <116361701+Shivam1Singh1@users.noreply.github.com>
2024-07-07 12:08:23 +0530

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -14,4 +14,4 @@ export const assets ={
     parcel_icon
 }
 
-export const url = 'https://food-delivery-application-gqqj.onrender.com'
+export const url = 'https://food-delivery-app-clnc.onrender.com'
